80887bc34142fefb813685bfef4a82990587c488
Flavio Castro <flcf@cesar.org.br>
2016-11-10 19:56:30 -0300

    Add server side requisition to reboot the gateway

--- app/routes/administration.js
+++ app/routes/administration.js
@@ -1,5 +1,6 @@
 var router = require('express').Router(); // eslint-disable-line new-cap
 var settings = require('../models/settings');
+var exec = require('child_process').exec;
 
 var get = function get(req, res) {
   settings.getAdministrationSettings(function onAdministrationSettingsReturned(err, admSettings) {
@@ -26,8 +27,19 @@ var post = function post(req, res) {
   });
 };
 
+var postReboot = function postReboot(req, res) {
+  exec('reboot', function reboot(error) {
+    if (error !== null) {
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(204);
+    }
+  });
+};
+
 router.get('/', get);
 router.post('/', post);
+router.post('/reboot', postReboot);
 
 module.exports = {
   router: router

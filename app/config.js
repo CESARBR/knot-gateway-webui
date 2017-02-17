e3b72e2136d19359d1abf8f390b3dd113528229d
Silvio Santana <shcs@cin.ufpe.br>
2017-02-17 14:53:55 -0300

    Setup database connection

--- app/config.js
+++ app/config.js
@@ -3,8 +3,12 @@ var CONFIGURATION_FILE = process.env.CONFIG_FILE || '/etc/knot/gatewayConfig.jso
 var PORT = process.env.PORT || 8080;
 var TOKEN_SECRET = process.env.TOKEN_SECRET || 'devsecret';
 var TOKEN_EXPIRATION = 120 * 60;
+var DB_URL = process.env.DB_URL || 'localhost';
+var DB_PORT = process.env.DB_PORT || '27017';
+var DB_NAME = process.env.DB_NAME || 'knot_fog';
 
 module.exports = {
+  DATABASE_URL: 'mongodb://' + DB_URL + ':' + DB_PORT + '/' + DB_NAME,
   DEVICES_FILE: DEVICES_FILE,
   CONFIGURATION_FILE: CONFIGURATION_FILE,
   PORT: PORT,
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var DATABASE_URL = require('./app/config').DATABASE_URL;
 
 var config = require('./app/config');
 var apiRoute = require('./app/routes/api');
@@ -15,6 +17,8 @@ serverConfig.use(express.static(publicRoot));
 serverConfig.use('/api', apiRoute.router);
 serverConfig.use('*', handlers.defaultHandler);
 serverConfig.use(handlers.errorHandler);
+
+mongoose.connect(DATABASE_URL);
 
 serverConfig.listen(config.PORT, function () {
   console.log('Listening on ' + config.PORT);

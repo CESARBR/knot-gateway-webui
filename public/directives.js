cd13c87f015d587e2e0a20a174d005c4739a20e7
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2017-08-17 14:25:13 -0300

    Remove ng suffix from ngConfirmClick
    ng suffix might give the idea this directive comes with the framework.

--- public/directives.js
+++ public/directives.js
@@ -2,15 +2,15 @@ var angular = require('angular');
 
 var appDirectives = angular.module('app.directives', []);
 
-appDirectives.directive('ngConfirmClick', [
-  function () {
+appDirectives.directive('confirmClick', [
+  function confirmClick() {
     return {
       priority: 1,
       terminal: true,
-      link: function (scope, element, attr) {
-        var msg = attr.ngConfirmClick || 'Are you sure?';
+      link: function link(scope, element, attr) {
+        var msg = attr.confirmClick || 'Are you sure?';
         var clickAction = attr.ngClick;
-        element.bind('click', function (/* event */) {
+        element.bind('click', function onClick(/* event */) {
           if (window.confirm(msg)) {
             scope.$eval(clickAction);
           }

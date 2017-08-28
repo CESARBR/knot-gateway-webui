a3b5e2ffbee9ce934e159d2e1d5981e53c128588
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2017-08-28 18:45:47 -0300

    Change method of stopping click propagation in confirmClick
    The directive was stopping click propagation by declaring it as a
    terminal directive and when the user confirmed the dialog, it would
    evaluate the ng-click content. This blocks any other directive applied
    to the button to process.
    
    Now, when the user cancels the dialog, only the click event is stopped
    from propagating, and other directives can execute without problems.

--- public/directives.js
+++ public/directives.js
@@ -5,14 +5,13 @@ var appDirectives = angular.module('app.directives', []);
 appDirectives.directive('confirmClick', [
   function confirmClick() {
     return {
-      priority: 1,
-      terminal: true,
+      priority: -1,
       link: function link(scope, element, attr) {
         var msg = attr.confirmClick || 'Are you sure?';
-        var clickAction = attr.ngClick;
-        element.bind('click', function onClick(/* event */) {
-          if (window.confirm(msg)) {
-            scope.$eval(clickAction);
+        element.bind('click', function onClick(event) {
+          if (!window.confirm(msg)) {
+            event.stopImmediatePropagation();
+            event.preventDefault();
           }
         });
       }

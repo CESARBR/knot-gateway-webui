1780f0b661d886d43e0eed464bd2aa8db10cd87d
Ramon Ribeiro <rhpr@cesar.org.br>
2018-03-19 15:49:17 -0300

    Update backend to list devices using new D-Bus API

--- app/services/devices.js
+++ app/services/devices.js
@@ -1,8 +1,11 @@
-var fs = require('fs');
-var dbus = require('dbus-native');
-var config = require('config');
+var dbusDeprecated = require('dbus-native');
+var dbus = require('dbus');
+var _ = require('lodash');
 
-var DEVICES_FILE = config.get('nrfd.devicesFile');
+var SERVICE_NAME = 'br.org.cesar.knot';
+var OBJECT_MANAGER_INTERFACE = 'org.freedesktop.DBus.ObjectManager';
+var DEVICE_INTERFACE = 'br.org.cesar.knot.Device1';
+var OBJECT_PATH = '/';
 
 var DevicesServiceError = function DevicesServiceError(message) {
   this.name = 'DevicesServiceError';
@@ -14,8 +17,13 @@ DevicesServiceError.prototype = Object.create(Error.prototype);
 DevicesServiceError.prototype.constructor = DevicesServiceError;
 
 
-var parseDbusError = function handleDbusError(err) { // eslint-disable-line vars-on-top
-  console.log('Unknown error while communicating with devices service:', err); // eslint-disable-line no-console
+var parseDbusError = function parseDbusError(err) { // eslint-disable-line vars-on-top, no-unused-vars, max-len
+  /**
+   * FIXME: The bellow console.log method call is raising TypeError,
+   * this can be happening due the dbus package when then knotd service raises an error
+   */
+  // console.log('Unknown error while communicating with devices service:', err);
+  console.log('Unknown error while communicating with devices service'); // eslint-disable-line no-console
   return new DevicesServiceError('Devices service is unavailable');
 };
 
@@ -23,101 +31,48 @@ var parseDbusError = function handleDbusError(err) { // eslint-disable-line vars
 var DevicesService = function DevicesService() { // eslint-disable-line vars-on-top
 };
 
-function getAllowedDevices(done) {
-  fs.readFile(DEVICES_FILE, 'utf8', function onRead(readErr, data) {
-    var file;
-
-    if (readErr) {
-      done(readErr);
-      return;
-    }
-
-    try {
-      file = JSON.parse(data);
-
-      done(null, file.keys);
-    } catch (parseErr) {
-      done(parseErr);
-    }
-  });
+function setKeysToLowerCase(obj) {
+  return _.mapKeys(obj, function onMapKeys(v, k) { return k.toLowerCase(); });
 }
 
-function getNearbyDevices(done) {
-  var sysbus = dbus.systemBus();
-  sysbus.invoke({
-    path: '/org/cesar/knot/nrf0',
-    destination: 'org.cesar.knot.nrf',
-    interface: 'org.cesar.knot.nrf0.Adapter',
-    member: 'GetBroadcastingDevices',
-    signature: '',
-    body: [],
-    type: dbus.messageType.methodCall
-  }, function onResult(dbusErr, res) {
-    var devices;
-    var devicesErr;
-
-    if (dbusErr) {
-      devicesErr = parseDbusError(dbusErr);
-      done(devicesErr);
-      return;
-    }
-
-    try {
-      devices = JSON.parse(res);
-      done(null, devices);
-    } catch (parseErr) {
-      done(parseErr);
-    }
-  });
-}
-
-function setAllowed(devices, allowed) {
-  devices.forEach(function onEntry(device) {
-    device.allowed = allowed;
-  });
-}
-
-function mergeDevicesLists(allowedList, nearbyList) {
-  // allowed - nearby
-  allowedList.forEach(function onEntry(device) {
-    var deviceIdx = nearbyList.findIndex(function isSameDevice(nearbyDevice) {
-      return nearbyDevice.mac === device.mac;
-    });
-    if (deviceIdx !== -1) {
-      nearbyList.splice(deviceIdx, 1);
-    }
-  });
-
-  // allowed (union) nearby
-  return allowedList.concat(nearbyList);
+/**
+ * The bellow method return a dictionary, which the key is the dbus object path
+ * and its value is another dictionary with the dbus interface as a key and
+ * the properties of that interface as value.
+ * We're just interested in the properties of devices interface so we need to
+ * filter the devices from the dbus's result
+ */
+function mapObjectsToDevices(objects) {
+  return _.chain(objects)
+    .pickBy(function onPick(object) { return _.has(object, DEVICE_INTERFACE); })
+    .map(function onMap(object) { return setKeysToLowerCase(object[DEVICE_INTERFACE]); })
+    .value();
 }
 
 DevicesService.prototype.list = function list(done) {
-  getAllowedDevices(function onAllowedDevices(allowedDevicesErr, allowedDevices) {
-    if (allowedDevicesErr) {
-      done(allowedDevicesErr);
+  var bus = dbus.getBus('system');
+  bus.getInterface(SERVICE_NAME, OBJECT_PATH, OBJECT_MANAGER_INTERFACE, function onInterface(getInterfaceErr, iface) { // eslint-disable-line max-len
+    var devicesErr;
+    if (getInterfaceErr) {
+      devicesErr = parseDbusError(getInterfaceErr);
+      done(devicesErr);
       return;
     }
-
-    setAllowed(allowedDevices, true); // eslint-disable-line no-param-reassign
-
-    getNearbyDevices(function onNearbyDevices(nearbyDevicesErr, nearbyDevices) {
-      var devices;
-      if (nearbyDevicesErr) {
-        done(nearbyDevicesErr);
+    iface.GetManagedObjects(function onManagedObject(getManagedObjectsErr, objects) { // eslint-disable-line new-cap, max-len
+      var devices = [];
+      if (getManagedObjectsErr) {
+        devicesErr = parseDbusError(getManagedObjectsErr);
+        done(devicesErr);
         return;
       }
-
-      setAllowed(nearbyDevices, false); // eslint-disable-line no-param-reassign
-
-      devices = mergeDevicesLists(allowedDevices, nearbyDevices);
+      devices = mapObjectsToDevices(objects);
       done(null, devices);
     });
   });
 };
 
 function addDevice(device, done) {
-  var sysbus = dbus.systemBus();
+  var sysbus = dbusDeprecated.systemBus();
   device.key = '';
   sysbus.invoke({
     path: '/org/cesar/knot/nrf0',
@@ -126,7 +81,7 @@ function addDevice(device, done) {
     member: 'AddDevice',
     signature: 'sss',
     body: [device.mac, device.key, device.name],
-    type: dbus.messageType.methodCall
+    type: dbusDeprecated.messageType.methodCall
   }, function onUpsert(dbusErr, upserted) {
     var devicesErr;
 
@@ -141,7 +96,7 @@ function addDevice(device, done) {
 }
 
 function removeDevice(device, done) {
-  var sysbus = dbus.systemBus();
+  var sysbus = dbusDeprecated.systemBus();
   sysbus.invoke({
     path: '/org/cesar/knot/nrf0',
     destination: 'org.cesar.knot.nrf',
@@ -149,7 +104,7 @@ function removeDevice(device, done) {
     member: 'RemoveDevice',
     signature: 's',
     body: [device.mac],
-    type: dbus.messageType.methodCall
+    type: dbusDeprecated.messageType.methodCall
   }, function onRemove(dbusErr, removed) {
     var devicesErr;
 

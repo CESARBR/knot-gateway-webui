888097511763574da88fca976d4518da8f72c669
Ramon Ribeiro <rhpr@cesar.org.br>
2018-03-19 15:49:17 -0300

    Update backend to list devices using new D-Bus API

---
+++ app/dbus.js
@@ -0,0 +1,24 @@
+var dbus = require('dbus');
+var bus = null;
+
+function getBus() {
+  if (!bus) {
+    bus = dbus.getBus('system');
+  }
+  return bus;
+}
+
+var parseDbusError = function parseDbusError(err, ErrorType, msg) { // eslint-disable-line vars-on-top, no-unused-vars, max-len
+  /**
+   * FIXME: The bellow console.log method call is raising TypeError,
+   * this can be happening due the dbus package when then knotd service raises an error
+   */
+  // console.log('Unknown error while communicatng with service:', err);
+  console.log('Unknown error while communicating with service'); // eslint-disable-line no-console
+  return new ErrorType(msg);
+};
+
+module.exports = {
+  getBus: getBus,
+  parseDbusError: parseDbusError
+};

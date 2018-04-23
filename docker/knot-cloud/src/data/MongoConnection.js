aa921143b9d657021d324004ba2c4228cfa8d2fe
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-04-23 19:46:06 -0300

    Add initial cloud mock
    This commit includes the POST /devices mock and project structure.

---
+++ docker/knot-cloud/src/data/MongoConnection.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+class MongoConnection {
+  constructor(host, port, databaseName) {
+    this.host = host;
+    this.port = port;
+    this.databaseName = databaseName;
+  }
+
+  async start() {
+    const uri = this.getDatabaseUri();
+    this.connection = await mongoose.connect(uri);
+  }
+
+  async count(name, schema, query) {
+    const Model = this.connection.model(name, schema);
+    return Model.count(query).exec();
+  }
+
+  async save(name, schema, data) {
+    const Model = this.connection.model(name, schema);
+    const model = new Model(data);
+    return model.save();
+  }
+
+  getDatabaseUri() {
+    return `mongodb://${this.host}:${this.port}/${this.databaseName}`;
+  }
+}
+
+export default MongoConnection;

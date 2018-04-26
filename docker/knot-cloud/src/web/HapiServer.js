72646c812aae6cafb1fa716b65b674ef4b96765a
Thiago Figueredo Cardoso <thiago.figueredo@cesar.org.br>
2018-04-25 22:09:03 -0300

    Add endpoint to cloud mock to list devices
    Change-Id: I5045c831695727a6f0abf407efaa29f50404f598

--- docker/knot-cloud/src/web/HapiServer.js
+++ docker/knot-cloud/src/web/HapiServer.js
@@ -38,6 +38,11 @@ class HapiServer {
         path: '/devices/{id}',
         handler: this.getDeviceHandler.bind(this),
       },
+      {
+        method: 'GET',
+        path: '/devices',
+        handler: this.listDevicesHandler.bind(this),
+      },
     ];
   }
 
@@ -63,6 +68,15 @@ class HapiServer {
     try {
       const device = await this.cloudApi.getDevice(request.params.id);
       return h.response(device).code(200);
+    } catch (err) {
+      return this.handleError(err, h);
+    }
+  }
+
+  async listDevicesHandler(request, h) {
+    try {
+      const devices = await this.cloudApi.listDevices(request.query);
+      return h.response(devices).code(200);
     } catch (err) {
       return this.handleError(err, h);
     }

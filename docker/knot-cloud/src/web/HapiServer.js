723c5f3809b42a5bd08ee16e42ac37c412b4b09c
Thiago Figueredo Cardoso <thiago.figueredo@cesar.org.br>
2018-04-25 22:08:44 -0300

    Add initial cloud mock
    This commit includes the POST /devices mock and project structure.
    
    Change-Id: I8b4491fa027679a51c7808cd4d30fc318d0f7cb6

---
+++ docker/knot-cloud/src/web/HapiServer.js
@@ -0,0 +1,55 @@
+import hapi from 'hapi';
+
+import EntityExistsError from 'domain/interactor/EntityExistsError';
+
+class HapiServer {
+  constructor(cloudApi) {
+    this.cloudApi = cloudApi;
+  }
+
+  async start(port) {
+    const server = hapi.server({
+      port,
+      router: {
+        stripTrailingSlash: true,
+        isCaseSensitive: false,
+      },
+    });
+
+    const routes = await this.createRoutes();
+    await server.route(routes);
+    await server.start();
+  }
+
+  async createRoutes() {
+    return [
+      {
+        method: 'POST',
+        path: '/devices',
+        handler: this.createDeviceHandler.bind(this),
+      },
+    ];
+  }
+
+  async createDeviceHandler(request, h) {
+    try {
+      const device = await this.cloudApi.createDevice(request.payload);
+      return h.response(device).code(201);
+    } catch (err) {
+      return this.handleError(err, h);
+    }
+  }
+
+  handleError(err, h) {
+    const errorObj = {
+      message: err.message,
+    };
+    if (err instanceof EntityExistsError) {
+      return h.response(errorObj).code(403);
+    }
+
+    return h.response(errorObj).code(err.code || 400);
+  }
+}
+
+export default HapiServer;

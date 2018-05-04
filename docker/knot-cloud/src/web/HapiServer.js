a26086894eadaf36d92773dede2120306df7a269
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-05-04 17:16:04 -0300

    Add endpoint to cloud mock to remove a device
    Change-Id: Iefe7aefd289cfbeb70f8b87aec47947cc8f9a43a

--- docker/knot-cloud/src/web/HapiServer.js
+++ docker/knot-cloud/src/web/HapiServer.js
@@ -1,6 +1,7 @@
 import hapi from 'hapi';
 
 import EntityExistsError from 'domain/interactor/EntityExistsError';
+import EntityNotFoundError from 'domain/interactor/EntityNotFoundError';
 
 class HapiServer {
   constructor(cloudApi) {
@@ -28,6 +29,11 @@ class HapiServer {
         path: '/devices',
         handler: this.createDeviceHandler.bind(this),
       },
+      {
+        method: 'DELETE',
+        path: '/devices/{id}',
+        handler: this.removeDeviceHandler.bind(this),
+      },
     ];
   }
 
@@ -40,10 +46,27 @@ class HapiServer {
     }
   }
 
+  async removeDeviceHandler(request, h) {
+    try {
+      await this.cloudApi.removeDevice(request.params.id);
+      return h.response().code(200);
+    } catch (err) {
+      if (err instanceof EntityNotFoundError) {
+        // DELETE /devices/{id} returns 400 instead 404 when device isn't found
+        const errorObj = this.mapErrorToJson(err);
+        return h.response(errorObj).code(400);
+      }
+      return this.handleError(err, h);
+    }
+  }
+
   handleError(err, h) {
     const errorObj = this.mapErrorToJson(err);
     if (err instanceof EntityExistsError) {
       return h.response(errorObj).code(403);
+    }
+    if (err instanceof EntityNotFoundError) {
+      return h.response(errorObj).code(404);
     }
 
     return h.response(errorObj).code(err.code || 400);

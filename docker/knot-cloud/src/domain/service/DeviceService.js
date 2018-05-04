533bea9680f30cd2e91e6486904a984d660a53e1
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-05-04 17:16:04 -0300

    Add endpoint to cloud mock to remove a device
    Change-Id: Iefe7aefd289cfbeb70f8b87aec47947cc8f9a43a

--- docker/knot-cloud/src/domain/service/DeviceService.js
+++ docker/knot-cloud/src/domain/service/DeviceService.js
@@ -1,10 +1,15 @@
 class DeviceService {
-  constructor(createDevice) {
+  constructor(createDevice, removeDevice) {
     this.createDevice = createDevice;
+    this.removeDevice = removeDevice;
   }
 
   async create(device) {
     return this.createDevice.execute(device);
+  }
+
+  async remove(uuid) {
+    await this.removeDevice.execute(uuid);
   }
 }
 

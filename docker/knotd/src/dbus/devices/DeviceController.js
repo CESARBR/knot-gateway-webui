f54730f5b916bf66b6cc43aedfc4d65fc8b9db87
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-06-12 17:00:58 -0300

    Add TestDevice1.Register() to the knotd mock
    Register() simulates the registration process that happens in knotd. It
    will create a device on the cloud and update the related properties.
    
    Change-Id: Ic9fafa06cc0cc7d1333b5876a146399abdfd4045

--- docker/knotd/src/dbus/devices/DeviceController.js
+++ docker/knotd/src/dbus/devices/DeviceController.js
@@ -1,12 +1,20 @@
 import { Subject } from 'rxjs';
 
 class DeviceController {
-  constructor(getDevice, pairDevice, forgetDevice, connectDevice, disconnectDevice) {
+  constructor(
+    getDevice,
+    pairDevice,
+    forgetDevice,
+    connectDevice,
+    disconnectDevice,
+    registerDevice,
+  ) {
     this.getDevice = getDevice;
     this.pairDevice = pairDevice;
     this.forgetDevice = forgetDevice;
     this.connectDevice = connectDevice;
     this.disconnectDevice = disconnectDevice;
+    this.registerDevice = registerDevice;
     this.propertiesChangedSource = new Subject();
   }
 
@@ -51,6 +59,11 @@ class DeviceController {
 
   async disconnect(id) {
     const changes = await this.disconnectDevice.execute(id);
+    this.propertiesChangedSource.next({ id, changes });
+  }
+
+  async register(id) {
+    const changes = await this.registerDevice.execute(id);
     this.propertiesChangedSource.next({ id, changes });
   }
 }

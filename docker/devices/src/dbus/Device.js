e97c273df29a870f0804a2cc624c374fdacf02cd
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-06-15 00:12:18 -0300

    Add forget command to devices app
    Change-Id: I0db11e0197cc4c03348f18953d8da9ac0fc9a32c

--- docker/devices/src/dbus/Device.js
+++ docker/devices/src/dbus/Device.js
@@ -3,6 +3,7 @@ import { promisify } from 'util';
 class Device {
   constructor(deviceInterface) {
     this.pair = promisify(deviceInterface.Pair.bind(deviceInterface));
+    this.forget = promisify(deviceInterface.Forget.bind(deviceInterface));
   }
 }
 

61256d7d3f08763630ece80bf7efcebf9cf43398
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-06-21 16:49:32 -0300

    Interpret knotd errors
    Due to a bug in the D-Bus library, all knotd errors were being handled
    as unexpected errors, generating an Internal Server Error (500) to the
    front-end. This patch interprets the errors, mapping it differently in
    each case:
    - Errors due to invalid state for the given operation are handled as a
    success, e.g. pair on a paired device
    - Errors due to an operation in progress generate Forbidden (403)
    - Other errors generate an Internal Server Error (500)
    
    Change-Id: I04283149e1c9128dff8523966d236d090c7800d9

--- app/services/knot.js
+++ app/services/knot.js
@@ -4,10 +4,6 @@ var SERVICE_NAME = 'br.org.cesar.knot';
 var INTERFACE_NAME = 'br.org.cesar.knot.Settings1';
 var OBJECT_PATH = '/';
 
-// KnotService is the KNoT Daemon (knotd)
-var KnotService = function KnotService() {
-};
-
 var KnotServiceError = function KnotServiceError(message) {
   this.name = 'KnotServiceError';
   this.message = message;
@@ -17,26 +13,36 @@ var KnotServiceError = function KnotServiceError(message) {
 KnotServiceError.prototype = Object.create(Error.prototype);
 KnotServiceError.prototype.constructor = KnotServiceError;
 
+
+// KnotService is the KNoT Daemon (knotd)
+var KnotService = function KnotService() { // eslint-disable-line vars-on-top
+};
+
+var parseDbusError = function parseDbusError(err, msg) { // eslint-disable-line vars-on-top
+  console.log('Unknown error while communicating with KNoT service:', err); // eslint-disable-line no-console, max-len
+  return new KnotServiceError(msg);
+};
+
 KnotService.prototype.setUserCredentials = function setUserCredentials(settings, done) {
   var bus = dbus.getBus();
 
-  bus.getInterface(SERVICE_NAME, OBJECT_PATH, INTERFACE_NAME, function onInterfaceGet(err, iface) {
-    var dbusError;
-    if (err) {
-      dbusError = dbus.parseDbusError(err, KnotServiceError, 'KNoT service is unavailable');
-      done(dbusError);
+  bus.getInterface(SERVICE_NAME, OBJECT_PATH, INTERFACE_NAME, function onInterfaceGet(getInterfaceErr, iface) { // eslint-disable-line max-len
+    var err;
+    if (getInterfaceErr) {
+      err = parseDbusError(getInterfaceErr, 'KNoT service is unavailable');
+      done(err);
       return;
     }
     iface.setProperty('Uuid', settings.uuid, function onUuidPropertySet(setUuidPropertyErr) {
       if (setUuidPropertyErr) {
-        dbusError = dbus.parseDbusError(setUuidPropertyErr, KnotServiceError, 'KNoT service is unavailable');
-        done(dbusError);
+        err = parseDbusError(setUuidPropertyErr, 'KNoT service is unavailable');
+        done(err);
         return;
       }
       iface.setProperty('Token', settings.token, function onTokenPropertySet(setTokenPropertyErr) {
         if (setTokenPropertyErr) {
-          dbusError = dbus.parseDbusError(setTokenPropertyErr, KnotServiceError, 'KNoT service is unavailable');
-          done(dbusError);
+          err = parseDbusError(setTokenPropertyErr, 'KNoT service is unavailable');
+          done(err);
           return;
         }
         done(null);

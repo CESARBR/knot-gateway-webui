1e8b26992ce8d6b692d6650abfd11c61cc88f3d1
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-06-28 18:57:45 -0300

    Add favicons
    Change-Id: I8c82c66ad71e5d060c68ba592666aa41416e5367

---
+++ public/img/favicons/index.js
@@ -0,0 +1,6 @@
+var faviconsContext = require.context(
+  '!!file-loader?name=[name].[ext]!.',
+  true,
+  /\.(svg|png|ico|xml|json|webmanifest)$/
+);
+faviconsContext.keys().forEach(faviconsContext);

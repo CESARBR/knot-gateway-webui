dd3e3db01319055d47f9f985e63b7872a6cbd0cb
João Neto <joao.alexandre@cesar.org.br>
2018-10-01 15:40:28 -0300

    Update cloud model to support FIWARE settings
    This patch create specific models for each cloud settings.
    
    Change-Id: I381de0815e544766375be943c34789f8e94f80b4
    Reviewed-on: https://review.gerrithub.io/427549
    Reviewed-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>
    Tested-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>

--- app/models/cloud.js
+++ app/models/cloud.js
@@ -1,23 +1,51 @@
 var mongoose = require('mongoose');
 
-var cloudSchema = new mongoose.Schema({
-  platform: String,
+var options = { discriminatorKey: 'type' };
+
+var settingsSchema = new mongoose.Schema({
+  platform: String
+}, options);
+
+var meshbluSettingsSchema = new mongoose.Schema({
   hostname: String,
   port: Number
-});
-var Cloud = mongoose.model('cloud', cloudSchema);
+}, options);
+
+var fiwareSettingsSchema = new mongoose.Schema({
+  iota: {
+    hostname: String,
+    port: Number
+  },
+  orion: {
+    hostname: String,
+    port: Number
+  }
+}, options);
+
+var Settings = mongoose.model('Settings', settingsSchema);
+var MeshbluSettings = Settings.discriminator('MeshbluSettings', meshbluSettingsSchema);
+var FiwareSettings = Settings.discriminator('FiwareSettings', fiwareSettingsSchema);
 
 var getCloudSettings = function getCloudSettings(done) {
-  Cloud.findOne({}, done);
+  Settings.findOne({}, done);
 };
 
 var setCloudSettings = function setCloudSettings(settings, done) {
-  Cloud.findOneAndUpdate({}, settings, { upsert: true }, done);
+  Settings.deleteOne({}, function onOlderStateRemoved(err) {
+    if (err) {
+      done(err);
+      return;
+    } else if (settings.platform === 'MESHBLU') {
+      MeshbluSettings.findOneAndUpdate({}, settings, { upsert: true }, done);
+    } else if (settings.platform === 'FIWARE') {
+      FiwareSettings.findOneAndUpdate({}, settings, { upsert: true }, done);
+    }
+  });
 };
 
 var existsCloudSettings = function existsCloudSettings(done) {
   getCloudSettings(function onCloudSettings(err, settings) {
-    done(err, !!settings && settings.platform && settings.hostname && settings.port);
+    done(err, !!settings);
   });
 };
 

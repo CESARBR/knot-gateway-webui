ca73d8a712d68e153782d060056fe1347c36dda5
João Neto <joao.alexandre@cesar.org.br>
2018-10-01 16:01:09 -0300

    Enable backend to configure FIWARE cloud
    Change-Id: I7c7be5e210d468a06075d183b3c51fb6c3a4b49c

--- app/schemas/cloud.js
+++ app/schemas/cloud.js
@@ -1,6 +1,16 @@
 var joi = require('celebrate').Joi;
 var supportedPlatforms = ['MESHBLU', 'FIWARE'];
 
+var hostname = joi
+  .string()
+  .hostname()
+  .required();
+
+var port = joi
+  .number()
+  .integer().min(1).max(65535)
+  .required();
+
 var update = {
   platform: joi
     .string()
@@ -9,11 +19,23 @@ var update = {
   hostname: joi
     .string()
     .hostname()
-    .required(),
+    .when('platform', { is: 'MESHBLU', then: joi.required() }),
   port: joi
     .number()
     .integer().min(1).max(65535)
-    .required()
+    .when('platform', { is: 'MESHBLU', then: joi.required() }),
+  iota: joi
+    .object({
+      hostname: hostname,
+      port: port
+    })
+    .when('platform', { is: 'FIWARE', then: joi.required() }),
+  orion: joi
+    .object({
+      hostname: hostname,
+      port: port
+    })
+    .when('platform', { is: 'FIWARE', then: joi.required() })
 };
 
 module.exports = {

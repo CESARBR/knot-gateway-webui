de8d6ff249959b2b0e5be5b95ced69cc5f44a784
João Neto <joao.alexandre@cesar.org.br>
2018-10-18 18:48:11 -0300

    Enable backend to configure cloud security
    Change-Id: I0b42496a1e573f246461d5c13aefe94a83b18378
    Reviewed-on: https://review.gerrithub.io/429978
    Reviewed-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>
    Tested-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>

--- app/routes/cloud.js
+++ app/routes/cloud.js
@@ -7,7 +7,9 @@ var cloudCtrl = require('../controllers/cloud');
 var cloudSchemas = require('../schemas/cloud');
 
 router.get('/', state.skipWhenConfiguration(auth.authorize()), cloudCtrl.get);
+router.get('/security', state.skipWhenConfiguration(auth.authorize()), cloudCtrl.getSecurity);
 router.put('/', state.onlyWhenConfigurationCloud, celebrate({ body: cloudSchemas.update }), cloudCtrl.update);
+router.put('/security', state.onlyWhenConfigurationCloudSecurity, celebrate({ body: cloudSchemas.updateSecurity }), cloudCtrl.updateSecurity);
 
 module.exports = {
   router: router

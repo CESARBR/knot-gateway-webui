c1ae5bcd55734fcc68985d263cd6b5b798aee300
Ramon Ribeiro <rhpr@cesar.org.br>
2019-01-25 09:32:42 -0300

    Add activate gateway endpoint to backend
    On HTTP PUT request to /api/cloud/gateway/<uuid>/attrs/active, the
    gateway with the corresponding uuid is activated on KNoT Cloud.
    
    Change-Id: I3f6cb9fb61a19b1db42010e8170e65562be87f35
    Reviewed-on: https://review.gerrithub.io/c/442621
    Reviewed-by: Paulo Serra Filho <ptvsf@cesar.org.br>
    Reviewed-by: João Neto <joao.alexandre@cesar.org.br>
    Tested-by: Paulo Serra Filho <ptvsf@cesar.org.br>

--- app/controllers/cloud.js
+++ app/controllers/cloud.js
@@ -99,6 +99,40 @@ var createGateway = function createGateway(req, res, next) {
   });
 };
 
+var activateGateway = function activateGateway(req, res, next) {
+  var uuid = req.params.id;
+  var cloudService;
+  cloud.getCloudSettings(function onCloudSettings(err, settings) {
+    if (err) {
+      next(err);
+    } else {
+      users.getUser(function onUser(userErr, user) {
+        if (userErr) {
+          next(userErr);
+        } else {
+          cloudService = new CloudService(settings.authenticator, settings.meshblu);
+          cloudService.activateGateway(user, uuid, function onActivate(activateErr, token) {
+            if (activateErr) {
+              next(activateErr);
+            } else {
+              finishSetup(settings.platform, settings.meshblu, {
+                uuid: uuid,
+                token: token
+              }, function onSetupDone(finishSetupErr) {
+                if (finishSetupErr) {
+                  next(finishSetupErr);
+                } else {
+                  res.end();
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+};
+
 var update = function update(req, res, next) {
   cloud.setCloudSettings(req.body, function onCloudSettingsSet(setCloudErr) {
     var connectorSvc = new ConnectorService();
@@ -151,5 +185,6 @@ module.exports = {
   listGateways: listGateways,
   update: update,
   updateSecurity: updateSecurity,
-  createGateway: createGateway
+  createGateway: createGateway,
+  activateGateway: activateGateway
 };

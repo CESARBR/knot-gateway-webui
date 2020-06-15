d131dd8e1b7c35976dc97d6ada89b6c027aeb153
Igor Albanezi Conceição <iac@cesar.org.br>
2020-06-15 12:02:47 -0300

    controller: Remove gateway setup

--- app/routes/cloud.js
+++ app/routes/cloud.js
@@ -8,11 +8,8 @@ var cloudSchemas = require('../schemas/cloud');
 
 router.get('/', state.skipWhenConfiguration(auth.authorize()), cloudCtrl.get);
 router.get('/security', state.skipWhenConfiguration(auth.authorize()), cloudCtrl.getSecurity);
-router.get('/gateways', state.onlyWhenConfigurationGateway, cloudCtrl.listGateways);
 router.put('/', state.onlyWhenConfigurationCloud, celebrate({ body: cloudSchemas.update }), cloudCtrl.update);
 router.put('/security', state.onlyWhenConfigurationCloudSecurity, celebrate({ body: cloudSchemas.updateSecurity }), cloudCtrl.updateSecurity);
-router.post('/gateway', state.onlyWhenConfigurationGateway, celebrate({ body: cloudSchemas.createGateway }), cloudCtrl.createGateway);
-router.put('/gateway/:id/attrs/active', state.onlyWhenConfigurationGateway, celebrate({ params: cloudSchemas.activateGateway }), cloudCtrl.activateGateway);
 
 module.exports = {
   router: router

3e0ef546f893c6b415991a1eda070150b34208da
João Neto <joao.alexandre@cesar.org.br>
2020-07-01 13:23:36 -0300

    fog: Add AMQP capabilities to mock service

--- docker/knot-fog/src/index.js
+++ docker/knot-fog/src/index.js
@@ -3,6 +3,9 @@ import config from 'config';
 import MongoConnection from 'data/MongoConnection';
 import MongoUserGateway from 'data/MongoUserGateway';
 
+import AMQPConnection from 'amqp/AMQPConnection';
+import MessageHandler from 'amqp/MessageHandler';
+
 import CreateUser from 'domain/interactor/CreateUser';
 import CreateToken from 'domain/interactor/CreateToken';
 import UserService from 'domain/service/UserService';
@@ -14,8 +17,14 @@ const DB_PORT = config.get('db.port');
 const DB_NAME = config.get('db.databaseName');
 const PORT = config.get('server.port');
 
-const connection = new MongoConnection(DB_HOST, DB_PORT, DB_NAME);
-const userGateway = new MongoUserGateway(connection);
+const AMQP_HOST = config.get('amqp.host');
+const AMQP_PORT = config.get('amqp.port');
+
+const mongoConnection = new MongoConnection(DB_HOST, DB_PORT, DB_NAME);
+const userGateway = new MongoUserGateway(mongoConnection);
+
+const amqpConnection = new AMQPConnection({ hostname: AMQP_HOST, port: AMQP_PORT });
+
 const createUser = new CreateUser(userGateway);
 const createToken = new CreateToken(userGateway);
 const userService = new UserService(createUser, createToken);
@@ -23,8 +32,16 @@ const fogApi = new FogApi(userService);
 const server = new HapiServer(fogApi);
 
 async function start() {
-  await connection.start();
+  const amqpChannel = await amqpConnection.start();
+  const messageHandler = new MessageHandler(
+    deviceService,
+    amqpConnection,
+    amqpChannel
+  );
+
+  await mongoConnection.start();
   await server.start(PORT);
+  await messageHandler.start();
 }
 
 start();
